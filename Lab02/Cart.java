1cdad17d006f4d9316193ca9df62438c727cde1f
Trí Thành Hoàng Kim <jot2003>
2023-10-31 23:53:28 +0700

    ...

---
+++ Lab02/Cart.java
@@ -0,0 +1,48 @@
+/**
+
+*/
+// package Lab02;
+
+public class Cart {
+    public static final int MAX_NUMBERS_OF = 20;
+    private DigitialVideoDisc itemsOrdered[] = new DigitialVideoDisc[MAX_NUMBERS_OF];
+    private int qtyOrdered;
+
+    public Cart() {
+        this.qtyOrdered = 0;
+    }
+
+    public void addDigitialVideoDisc(DigitialVideoDisc disc) {
+        if (qtyOrdered <= Cart.MAX_NUMBERS_OF) {
+            itemsOrdered[qtyOrdered] = disc;
+            qtyOrdered++;
+            System.out.println("The disc has been added");
+        } else System.out.println("The cart is almost full");
+    }
+
+    // remove any dvd from itemsOrdered.
+    public DigitialVideoDisc removeDigitialVideoDisc(DigitialVideoDisc disc) {
+        for (int i = 0; i < qtyOrdered; i++) {
+            if (itemsOrdered[i].equals(disc)) {
+                DigitialVideoDisc tmp = itemsOrdered[i];
+                for (int j = i; j < qtyOrdered; j++) {
+                    itemsOrdered[j] = itemsOrdered[j+1];
+                }
+                qtyOrdered--;
+                return tmp;
+            }
+        }
+
+        return null;
+    }
+
+    public float totalCost() {
+        float sum = 0;
+        for (int i = 0; i < qtyOrdered; i++) {
+            sum += itemsOrdered[i].getCost();
+        }
+
+        return sum;
+    }
+
+}
